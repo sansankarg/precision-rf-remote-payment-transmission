ba4f5958f215029018264904252a94e52034aeed
sansankarg <sansankarg@gmail.com>
2024-04-15 23:46:48 +0530

    database added

--- update_server.py
+++ update_server.py
@@ -1,5 +1,6 @@
 import requests
-id = "8220709131"
+
+userid = "8220709131"
 name = "8220709131"
-ar = 900
-requests.post(url = 'http://192.168.103.76:5000/transaction.html', json = {'id' : id, 'name' : name, 'ar' : ar})
+ar = 100
+requests.post(url='http://192.168.101.76:5000/transaction.html', json={'id': userid, 'name': name, 'ar': ar})
